fc789bdd0676d777590f2640f254f16c05829611
Rian Chakraborty <rian.chakraborty@student.tugraz.at>
2025-12-05 08:41:47 +0100

    Add Sphinx docs and docs CI pipeline

---
+++ docs/_build/html/_sources/api.rst.txt
@@ -0,0 +1,8 @@
+API Reference
+=============
+
+.. automodule:: packages_Q40.math_utils
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
---
+++ src/packages_Q40/math_utils.py
@@ -0,0 +1,10 @@
+"""Small math helper functions for the demo package."""
+
+def add(a: float, b: float) -> float:
+    """Return the sum of ``a`` and ``b``."""
+    return a + b
+
+
+def multiply(a: float, b: float) -> float:
+    """Return the product of ``a`` and ``b``."""
+    return a * b
